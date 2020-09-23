e2451761c3aeeab0b834568aec42c5a5d4908952
UNO User <unouser@unomaha.edu>
2020-09-23 12:37:05 -0500

    Initital Commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main(){
+        MyCollection<String> myCollection = new BinaryTree();
+        System.out.println(myCollection.size());
+        myCollection.add("mike");
+        System.out.println(myCollection.size());
+        myCollection.add("golf");
+        System.out.println(myCollection.size());
+        myCollection.add("sierra");
+        System.out.println(myCollection.size());
+        myCollection.add("alpha");
+        System.out.println(myCollection.size());
+    }
+}
